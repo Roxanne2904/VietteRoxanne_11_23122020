41266d436138fe17952573a5c7341d810e5a47e9
Roxanne <roxanne-viette@hotmail.fr>
2022-02-07 19:09:08 +0100

    modifications sur la page host

--- src/Pages/Host/index.jsx
+++ src/Pages/Host/index.jsx
@@ -5,7 +5,7 @@ import Carousel from '../../Components/Carousel'
 import Tags from '../../Components/Tags'
 import Stars from '../../Components/Stars'
 import Lists from '../../Components/Lists'
-
+import Error from '../Error'
 //Styled
 import { StyledGeneralIdContain } from '../../Styled/Pages/Host/index'
 import { StyledHostIdAndTagsContain } from '../../Styled/Pages/Host/index'
@@ -26,46 +26,51 @@ function HostPage(props) {
 
   hostDatas = datas !== null && datas.filter((data) => data.id.includes(id))
 
-  const rating = hostDatas !== false && parseInt(hostDatas[0].rating)
-  const description = hostDatas !== false && hostDatas[0].description
-  const equipments = hostDatas !== false && hostDatas[0].equipments
+  const rating =
+    hostDatas !== false &&
+    hostDatas.length !== 0 &&
+    parseInt(hostDatas[0].rating)
+  const description =
+    hostDatas !== false && hostDatas.length !== 0 && hostDatas[0].description
+  const equipments =
+    hostDatas !== false && hostDatas.length !== 0 && hostDatas[0].equipments
 
-  return (
-    hostDatas !== false && (
-      <main>
-        <Carousel />
-        <StyledGeneralIdContain>
-          <StyledHostIdAndTagsContain>
-            <div>
-              <StyledHostMainTitle>{`${hostDatas[0].title}`}</StyledHostMainTitle>
-              <StyledHostSubTitle>{`${hostDatas[0].location}`}</StyledHostSubTitle>
-            </div>
-            <Tags />
-          </StyledHostIdAndTagsContain>
-          <StyledProfilAndStarsContain>
-            <StyledHostIdProfil>
-              <StyledHostIdImg
-                src={`${hostDatas[0].host.picture}`}
-                alt={`${hostDatas[0].host.name}`}
-              />
-              <StyledHostNameContain>
-                <StyledHostFirstName>{`${
-                  hostDatas[0].host.name.split(' ')[0]
-                }`}</StyledHostFirstName>
-                <StyledHostName>{`${
-                  hostDatas[0].host.name.split(' ')[1]
-                }`}</StyledHostName>
-              </StyledHostNameContain>
-            </StyledHostIdProfil>
-            <Stars rating={rating} />
-          </StyledProfilAndStarsContain>
-        </StyledGeneralIdContain>
-        <StyledListContain>
-          <Lists contain={description} title="Description" page="host" />
-          <Lists contain={equipments} title="Equipements" page="host" />
-        </StyledListContain>
-      </main>
-    )
+  return hostDatas !== false && hostDatas.length !== 0 ? (
+    <main>
+      <Carousel />
+      <StyledGeneralIdContain>
+        <StyledHostIdAndTagsContain>
+          <div>
+            <StyledHostMainTitle>{`${hostDatas[0].title}`}</StyledHostMainTitle>
+            <StyledHostSubTitle>{`${hostDatas[0].location}`}</StyledHostSubTitle>
+          </div>
+          <Tags />
+        </StyledHostIdAndTagsContain>
+        <StyledProfilAndStarsContain>
+          <StyledHostIdProfil>
+            <StyledHostIdImg
+              src={`${hostDatas[0].host.picture}`}
+              alt={`${hostDatas[0].host.name}`}
+            />
+            <StyledHostNameContain>
+              <StyledHostFirstName>{`${
+                hostDatas[0].host.name.split(' ')[0]
+              }`}</StyledHostFirstName>
+              <StyledHostName>{`${
+                hostDatas[0].host.name.split(' ')[1]
+              }`}</StyledHostName>
+            </StyledHostNameContain>
+          </StyledHostIdProfil>
+          <Stars rating={rating} />
+        </StyledProfilAndStarsContain>
+      </StyledGeneralIdContain>
+      <StyledListContain>
+        <Lists contain={description} title="Description" page="host" />
+        <Lists contain={equipments} title="Equipements" page="host" />
+      </StyledListContain>
+    </main>
+  ) : (
+    <Error />
   )
 }
 
