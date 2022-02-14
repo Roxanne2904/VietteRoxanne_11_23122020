17daf44e4d3f36a36dfe55e881f2f28d4ebec245
Roxanne <roxanne-viette@hotmail.fr>
2022-02-14 13:41:22 +0100

    react router est dans un fichier séparé -App.jsx-

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+//Components
+import Header from './Components/Header/index'
+//Pages
+import Home from './Pages/Home/index'
+import Error from './Pages/Error/index'
+import HostPage from './Pages/Host/index'
+import About from './Pages/About'
+import Footer from './Components/Footer'
+//Context
+import { DatasProvider } from './Utils/Context/index'
+// basename={process.env.PUBLIC_URL}
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <DatasProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/host/:id" element={<HostPage hostDatas={null} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </DatasProvider>
+    </Router>
+  )
+}
+
+export default App
