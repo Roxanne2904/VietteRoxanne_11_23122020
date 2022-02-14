2d21a2fa3ebb732c333c97b336067d67502373e1
Roxanne <roxanne-viette@hotmail.fr>
2022-02-14 13:43:10 +0100

    nettoyage des commentaires

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom'
 import './css/style.css'
 import reportWebVitals from './reportWebVitals'
 import App from './App'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-// //Components
-// import Header from './Components/Header/index'
-// //Pages
-// import Home from './Pages/Home/index'
-// import Error from './Pages/Error/index'
-// import HostPage from './Pages/Host/index'
-// import About from './Pages/About'
-// import Footer from './Components/Footer'
-// //GlobalStyled
-// // import GlobalStyled from './Styled/Global/index'
-// //Context
-// import { DatasProvider } from './Utils/Context/index'
-// // basename={process.env.PUBLIC_URL}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
